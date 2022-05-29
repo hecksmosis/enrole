33dc0700aa726b503833622c6b71f343b2c4fe7b
pablo-garcia-ruiz <pgruiz727@gmail.com>
2022-05-29 17:29:45 +0200

    Add database

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var pool;
 if (isProduction) {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: { rejectUnauthorized: false }
     });
 } else {
     pool = new Pool({
